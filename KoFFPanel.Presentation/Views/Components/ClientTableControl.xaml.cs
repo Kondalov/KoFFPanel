bf2c519f2777253bd0063bad465cfe3f2bd27905
Nikolay <44981313+Kondalov@users.noreply.github.com>
2026-09-18 11:45:37 +0100

    Реализован реактивный алгоритм «живой» сортировки (Live Shaping): при включенном режиме «Онлайн сверху» только что подключившиеся пользователи мгновенно поднимаются в самый верх списка, а отключившиеся пользователи молниеносно перемещаются вниз без сброса фокуса или позиции прокрутки.

--- KoFFPanel.Presentation/Views/Components/ClientTableControl.xaml.cs
+++ KoFFPanel.Presentation/Views/Components/ClientTableControl.xaml.cs
@@ -22,27 +22,58 @@ namespace KoFFPanel.Presentation.Views.Components
                 var view = CollectionViewSource.GetDefaultView(dataGrid.ItemsSource);
                 if (view == null) return;
 
-                // Умная 3-позиционная сортировка:
-                // 1. Онлайн сверху (Descending)
-                // 2. Офлайн сверху (Ascending)
+                // Умная 3-позиционная реактивная сортировка в реальном времени:
+                // 1. Онлайн сверху (Descending): только что подключившиеся клиенты мгновенно поднимаются вверх, отключившиеся уходят вниз
+                // 2. Офлайн сверху (Ascending): отключенные клиенты сверху
                 // 3. Сброс к исходному порядку (null)
                 var currentDirection = e.Column.SortDirection;
                 if (currentDirection == null)
                 {
                     e.Column.SortDirection = ListSortDirection.Descending;
                     view.SortDescriptions.Clear();
+                    view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.IsOnline), ListSortDirection.Descending));
+                    view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.ConnectedAt), ListSortDirection.Descending));
                     view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.ActiveConnections), ListSortDirection.Descending));
+                    view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.LastOnline), ListSortDirection.Descending));
+
+                    if (view is ICollectionViewLiveShaping liveView && liveView.CanChangeLiveSorting)
+                    {
+                        liveView.LiveSortingProperties.Clear();
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.IsOnline));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.ConnectedAt));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.ActiveConnections));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.LastOnline));
+                        liveView.IsLiveSorting = true;
+                    }
                 }
                 else if (currentDirection == ListSortDirection.Descending)
                 {
                     e.Column.SortDirection = ListSortDirection.Ascending;
                     view.SortDescriptions.Clear();
+                    view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.IsOnline), ListSortDirection.Ascending));
                     view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.ActiveConnections), ListSortDirection.Ascending));
+                    view.SortDescriptions.Add(new SortDescription(nameof(VpnClient.LastOnline), ListSortDirection.Ascending));
+
+                    if (view is ICollectionViewLiveShaping liveView && liveView.CanChangeLiveSorting)
+                    {
+                        liveView.LiveSortingProperties.Clear();
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.IsOnline));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.ConnectedAt));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.ActiveConnections));
+                        liveView.LiveSortingProperties.Add(nameof(VpnClient.LastOnline));
+                        liveView.IsLiveSorting = true;
+                    }
                 }
                 else
                 {
                     e.Column.SortDirection = null;
                     view.SortDescriptions.Clear();
+
+                    if (view is ICollectionViewLiveShaping liveView && liveView.CanChangeLiveSorting)
+                    {
+                        liveView.IsLiveSorting = false;
+                        liveView.LiveSortingProperties.Clear();
+                    }
                 }
             }
         }
